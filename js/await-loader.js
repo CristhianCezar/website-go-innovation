4ca58fe86af048c802ce09ad7f5cac09898486c0
CristhianCezar <cristhiancezardev@gmail.com>
2023-03-23 15:09:09 -0300

    Loader concluded

---
+++ js/await-loader.js
@@ -0,0 +1,8 @@
+
+function awaitLoader() {
+    const headerLoading = document.querySelector('.header');
+    const loading =document.getElementById('loading');
+    
+    loading.style.display = "none";
+    headerLoading.style.opacity = "1";
+}
